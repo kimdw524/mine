dea2a72b6c36088bc8c53a3f53769a6a9b614916
권용진 <dydwls9703@naver.com>
2024-07-16 14:45:22 +0900

    be/chore: init websocket server
    웹소켓 서버 배포
    
    #S11P12D106-14 / #done / #8

---
+++ backend/socket/src/test/java/com/mine/socket/SocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
