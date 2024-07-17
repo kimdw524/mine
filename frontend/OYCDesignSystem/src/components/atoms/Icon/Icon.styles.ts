cc97b924f39ed215fc221bcd408024691cce8178
kimdw524 <kimdw524@naver.com>
2024-07-17 17:54:07 +0900

    fe/feat: Icon Component 추가
    Icon Component
    
    #17

---
+++ frontend/OYCDesignSystem/src/components/atoms/Icon/Icon.styles.ts
@@ -0,0 +1,19 @@
+import { Theme, css } from '@emotion/react';
+import { PaletteColor } from '../../../themes/lightTheme';
+import { Size } from '../../../themes/themeBase';
+
+const sizes: Record<Size, string> = {
+  sm: '1.25rem',
+  md: '1.5rem',
+  lg: '2rem',
+  xl: '2.5rem',
+};
+
+export const base = (theme: Theme, palette: PaletteColor, size: Size) => css`
+  color: ${palette.main};
+
+  svg {
+    width: ${sizes[size]};
+    height: ${sizes[size]};
+  }
+`;
