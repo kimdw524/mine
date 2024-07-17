83a073d8addf23b92794d07582bc99a3c8987645
kimdw524 <kimdw524@naver.com>
2024-07-17 12:59:10 +0900

    fe/feat: dark, light color palette 추가
    필요성을 느껴 추가함ript 버전이

--- frontend/OYCDesignSystem/src/themes/themeBase.ts
+++ frontend/OYCDesignSystem/src/themes/themeBase.ts
@@ -7,8 +7,6 @@ export type FontWeight = 'light' | 'medium' | 'bold';
 
 export type ThemeBaseType = {
   palette: Object;
-  //spacing: Record<Size, string>;
-  //borderRadius: Record<Size, string>;
   typography: {
     fontSize: Record<FontSize, string>;
     fontWeight: Record<FontWeight, number>;
@@ -17,18 +15,6 @@ export type ThemeBaseType = {
 
 const ThemeBase: ThemeBaseType = {
   palette: colorPalette,
-  /*spacing: {
-    sm: '0.75rem',
-    md: '0.875rem',
-    lg: '1rem',
-    xl: '1.25rem',
-  },
-  borderRadius: {
-    sm: '0.25rem',
-    md: '0.375rem',
-    lg: '0.5rem',
-    xl: '0.625rem',
-  },*/
   typography: {
     fontSize: {
       xs: '0.75rem',
