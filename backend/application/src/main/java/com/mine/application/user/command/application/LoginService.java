4bdbc84651d47c0e5d4892dea940090d06e8d7d2
김현진 <khj745700@naver.com>
2024-07-18 14:27:03 +0900

    be/feat: Login, Logout 추가 및 로그인 검증 AOP 기능 추가
    Login 기능 추가
     - email, password로 로그인 기능 작성
    
     Logout 추가
      - 세션에 이메일 정보를 지웁니다.
    
     로그인 검증 AOP
      - @LoginCheck로 세션에 이메일이 들어있는지 검증합니다.
    
     #16

---
+++ backend/application/src/main/java/com/mine/application/user/command/application/LoginService.java
@@ -0,0 +1,40 @@
+package com.mine.application.user.command.application;
+
+import com.mine.application.common.domain.SessionConstants;
+import com.mine.application.common.domain.SessionDao;
+import com.mine.application.user.command.domain.Password;
+import com.mine.application.user.command.domain.User;
+import com.mine.application.user.command.domain.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+    private final UserRepository userRepository;
+    private final SessionDao sessionDao;
+
+    public void login(LoginRequest loginRequest) {
+        Optional<User> findUser = userRepository.findByEmail(loginRequest.getEmail());
+        if (findUser.isPresent()) {
+            User user = findUser.get();
+            if (user.getPassword().equals(Password.of(loginRequest.getPassword(), false))) {
+                sessionDao.put(SessionConstants.EMAIL, loginRequest.getEmail());
+            }
+        }
+        // throw error;
+    }
+
+    public void loginCheck() {
+        // TODO : 예외 처리 필요
+        assert sessionDao.get(SessionConstants.EMAIL) != null;
+    }
+
+    public void logout() {
+        sessionDao.remove(SessionConstants.EMAIL);
+    }
+
+}
--- backend/application/src/main/java/com/mine/application/user/ui/AuthController.java
+++ backend/application/src/main/java/com/mine/application/user/ui/AuthController.java
@@ -1,12 +1,17 @@
 package com.mine.application.user.ui;
 
+import com.mine.application.user.command.application.LoginRequest;
+import com.mine.application.user.command.application.LoginService;
 import com.mine.application.user.command.application.SignupRequest;
 import com.mine.application.user.command.application.SignupService;
 import com.mine.application.user.query.UserQueryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
@@ -14,15 +19,29 @@ public class AuthController {
 
     private final SignupService signupService;
     private final UserQueryService userQueryService;
+    private final LoginService loginService;
 
     @PostMapping("/user")
-    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+    public ResponseEntity<?> signup(@RequestBody @Valid SignupRequest signupRequest) {
         signupService.signup(signupRequest);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/help/user/{userId}")
-    public ResponseEntity<?> existUserId(@PathVariable("userId") String userId) {
-        return ResponseEntity.ok().body(userQueryService.existUserId(userId));
+    @GetMapping("/help/user/{userEmail}")
+    public ResponseEntity<?> existuserEmail(@PathVariable("userEmail") String userEmail) {
+        return ResponseEntity.ok().body(userQueryService.existUserId(userEmail));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest loginRequest) {
+        loginService.login(loginRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<?> logout() {
+        loginService.logout();
+        return ResponseEntity.ok().build();
     }
 }
