7ee699b1a6a7302ff2ce938763e5715058626018
최요하 <yoha6865@gmail.com>
2024-07-18 15:30:59 +0900

    fe/fix: Typography storybook 문서 수정
    문서 상에서 바로 크기와 색을 확인할 수 있도록 수정
    
    children 을 추가하여 텍스트 직접 입력할 수 있도록 수정

--- frontend/OYCDesignSystem/src/components/atoms/Typography/Typography.stories.tsx
+++ frontend/OYCDesignSystem/src/components/atoms/Typography/Typography.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Typography } from './Typography';
 import { Palette } from '../../../themes/lightTheme';
+import { FontSize } from '../../../themes/themeBase';
 
 const meta = {
   title: 'UI/Atoms/Typography',
@@ -17,6 +18,9 @@ const meta = {
   ],
   tags: ['!autodocs'],
   argTypes: {
+    children: {
+      description: 'text를 입력해주세요',
+    },
     color: {
       description: '적용할 컬러 팔레트입니다.',
     },
@@ -35,8 +39,43 @@ type Story = StoryObj<typeof Typography>;
 
 export const Primary: Story = {
   args: {
+    children: 'Typography',
     color: 'primary',
     size: 'md',
     weight: 'medium',
   },
+};
+
+const colorList: Palette[] = ['primary', 'secondary', 'success', 'danger'];
+
+export const Default: Story = {
+  args: {
+    ...Primary.args,
+  },
+  render: (args) => (
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+      {colorList.map((color) => (
+        <Typography {...args} color={color}>
+          {color}
+        </Typography>
+      ))}
+    </div>
+  ),
+};
+
+const sizeList: FontSize[] = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+export const Sized: Story = {
+  args: {
+    ...Primary.args,
+  },
+  render: (args) => (
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+      {sizeList.map((s) => (
+        <Typography {...args} size={s}>
+          {Primary.args?.children}
+        </Typography>
+      ))}
+    </div>
+  ),
 };
