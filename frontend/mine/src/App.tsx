28abf87115f64c52e4d619925475dbb04fb6f306
kimdw524 <kimdw524@naver.com>
2024-07-19 17:47:39 +0900

    fe/feat: global style 추가
    add global style
    
    #23

--- frontend/mine/src/App.tsx
+++ frontend/mine/src/App.tsx
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
-
-interface Result {
-    id: string;
-    firstName: string;
-    lastName: string;
-}
+import React from 'react';
+import Signup from './pages/Signup';
+import { LightTheme } from 'oyc-ds';
+import { ThemeProvider } from '@emotion/react';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
-    const [result, setResult] = useState<Result>({
-        id: "",
-        firstName: "",
-        lastName: "",
-    });
-
-    useEffect(() => {
-        // mocking 한 데이터가 옴
-        fetch("http://localhost:3000/user")
-            .then((res) => res.json())
-            .then((res) => setResult({ ...res }));
-    }, []);
-
-    return (
-        <>
-            <div>{result.id}</div>
-            <div>{result.firstName}</div>
-            <div>{result.lastName}</div>
-        </>
-    );
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={LightTheme}>
+        <Signup />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
