b00ec81ab630b7af40a132619fc7bd1f552ef7ac
kimdw524 <kimdw524@naver.com>
2024-07-21 04:23:17 +0900

    fe/feat: component forwardRef 추가
    form에 사용되는 컴포넌트들은 ref가 필요해서 추가
    
    #33

--- frontend/OYCDesignSystem/src/components/molecules/Dropdown/Dropdown.tsx
+++ frontend/OYCDesignSystem/src/components/molecules/Dropdown/Dropdown.tsx
@@ -1,71 +1,79 @@
 /** @jsxImportSource @emotion/react */
-import React, { createContext, ReactNode, useCallback, useState } from 'react';
+import React, {
+  createContext,
+  forwardRef,
+  ReactNode,
+  useCallback,
+  useState,
+} from 'react';
 import { css, useTheme } from '@emotion/react';
 import { DropdownProps } from './Dropdown.types';
 import { base, variants, icon, itemContainer } from './Dropdown.styles';
-import { DropdownItem } from './DropdownItem';
 import { ReactComponent as Arrow } from '../../../assets/icons/arrow.svg';
 
 export const DropdownContext = createContext(
   (name: ReactNode, value: string) => {},
 );
 
-export const Dropdown = ({
-  children,
-  size = 'md',
-  color = 'primary',
-  variant = 'outlined',
-  disabled = false,
-  name,
-  onChange = (value: string) => {},
-  ...props
-}: DropdownProps) => {
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
-  const [selected, setSeleceted] = useState<{ name: ReactNode; value: string }>(
-    { name: '', value: '' },
-  );
+export const Dropdown = forwardRef<HTMLSelectElement, DropdownProps>(
+  ({
+    children,
+    size = 'md',
+    color = 'primary',
+    variant = 'outlined',
+    disabled = false,
+    name,
+    onChange = (value: string) => {},
+    ...props
+  }: DropdownProps) => {
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+    const [selected, setSeleceted] = useState<{
+      name: ReactNode;
+      value: string;
+    }>({ name: '', value: '' });
 
-  const handleClick = () => {
-    if (disabled) return;
-    setOpen((open) => !open);
-  };
+    const handleClick = () => {
+      if (disabled) return;
+      setOpen((open) => !open);
+    };
 
-  const handleUpdateValue = useCallback(
-    (name: ReactNode, value: string) => {
-      setSeleceted({ name, value });
-      onChange(value);
-    },
-    [onChange],
-  );
+    const handleUpdateValue = useCallback(
+      (name: ReactNode, value: string) => {
+        setSeleceted({ name, value });
+        onChange(value);
+      },
+      [onChange],
+    );
 
-  return (
-    <div
-      css={[
-        base(theme, size),
-        variants[variant](theme, theme.colors[color], open, disabled),
-      ]}
-      onClick={handleClick}
-      {...props}
-    >
-      <input type="hidden" name={name} value={selected.value} />
+    return (
       <div
-        css={css`
-          margin-right: 1.25rem;
-        `}
+        css={[
+          base(theme, size),
+          variants[variant](theme, theme.colors[color], open, disabled),
+        ]}
+        onClick={handleClick}
+        {...props}
       >
-        {selected.name}
+        <input type="hidden" name={name} value={selected.value} />
+        <div
+          css={css`
+            margin-right: 1.25rem;
+          `}
+        >
+          {selected.name}
+        </div>
+        <span css={icon(open)}>
+          <Arrow />
+        </span>
+        <div css={itemContainer(theme, !disabled && open)}>
+          <DropdownContext.Provider value={handleUpdateValue}>
+            {children}
+          </DropdownContext.Provider>
+        </div>
       </div>
-      <span css={icon(open)}>
-        <Arrow />
-      </span>
-      <div css={itemContainer(theme, !disabled && open)}>
-        <DropdownContext.Provider value={handleUpdateValue}>
-          {children}
-        </DropdownContext.Provider>
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
 
-Dropdown.Item = DropdownItem;
+Dropdown.displayName = 'Dropdown';
