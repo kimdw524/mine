b2832b2ead3e9ff67c567a265908a6605060ae80
kimdw524 <kimdw524@naver.com>
2024-07-22 16:55:31 +0900

    fe/chore: update eslint config
    update eslint config
    
    #23

--- frontend/mine/eslint.config.mjs
+++ frontend/mine/eslint.config.mjs
@@ -1,8 +1,7 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
-import { rules } from 'eslint-plugin-react/configs/all';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
