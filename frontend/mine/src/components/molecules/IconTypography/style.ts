ea7bd080218a7d208e94a324dd2828fec03aedda
최요하 <yoha6865@gmail.com>
2024-07-23 15:52:49 +0900

    fe/feat: 하단 메뉴 바에 사용될 IconTypography 컴포넌트 구현
    아이콘과 라벨을 갖는 컴포넌트 구현
    
    #21

--- frontend/mine/src/components/molecules/IconTypography/style.ts
+++ frontend/mine/src/components/molecules/IconTypography/style.ts
@@ -7,4 +7,17 @@ export const containerCss = css`
   row-gap: 0.25rem;
   pointer-events: none;
   user-select: none;
+  animation: grow 300ms ease-in-out;
+
+  @keyframes grow {
+    from {
+      transform: scale(0.9);
+      opacity: 0.5;
+    }
+
+    to {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
 `;
