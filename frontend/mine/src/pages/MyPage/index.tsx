68e8ee1f451694bdca8666d67c57aa2e6fb29ad4
최요하 <yoha6865@gmail.com>
2024-07-23 16:09:38 +0900

    fe/feat: 마이페이지 애니메이션 추가
    페이지 전환 애니메이션 추가
    
    #21

--- frontend/mine/src/pages/MyPage/index.tsx
+++ frontend/mine/src/pages/MyPage/index.tsx
@@ -4,6 +4,8 @@ import MenuBar from '../../components/organisms/MenuBar';
 import AppBar from '../../components/organisms/AppBar';
 import { containerCss } from './style';
 import UserInfo from './UserInfo';
+import TransitionAnimation from '../../components/common/TransitionAnimation';
+import styles from './MyPage.module.css';
 
 const MyPage = () => {
   const [curMenu, setCurMenu] = useState<number>(1);
@@ -15,13 +17,19 @@ const MyPage = () => {
           label="마이페이지"
           onBackClick={() => console.log('to main page')}
         />
-        {curMenu === 0 ? (
-          <div>it is Menu 0</div>
-        ) : curMenu === 1 ? (
-          <UserInfo />
-        ) : (
-          <div>it is Menu 2</div>
-        )}
+        <TransitionAnimation
+          data-key={curMenu.toString()}
+          className={{
+            normal: styles.fade,
+            enter: styles['fade-enter'],
+            exit: styles['fade-exit'],
+          }}
+        >
+          <div key={0}>it is Menu 0</div>
+          <UserInfo key={1} />
+          <div key={2}>it is Menu 2</div>
+        </TransitionAnimation>
+
         <MenuBar page="mypage" menu={curMenu} setCurMenu={setCurMenu} />
       </div>
     </>
