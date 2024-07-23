4f8a90fe945060b9fbaa281f98e3b2ee02610118
최요하 <yoha6865@gmail.com>
2024-07-23 15:47:41 +0900

    fe/feat: 응답에 매치되는 단어, 아이콘 얻기
    서버로부터 받은 응답에 맞는 한글 단어
     - EngToKor
    
    서버로부터 받은 응답에 맞는 아이콘
     - EngToIcon
    
    #21

--- frontend/mine/src/utils/EngToIcon.tsx
+++ frontend/mine/src/utils/EngToIcon.tsx
@@ -1,8 +1,23 @@
 import React, { ReactNode } from 'react';
-import { EnvelopeIcon, TagIcon, UserIcon } from '@heroicons/react/24/outline';
+import {
+  ArchiveBoxIcon,
+  EnvelopeIcon,
+  InformationCircleIcon,
+  TagIcon,
+  UserIcon,
+  ChatBubbleBottomCenterTextIcon,
+  HomeIcon,
+  MicrophoneIcon,
+} from '@heroicons/react/24/outline';
 
 export const engToIcon: Record<string, ReactNode> = {
   email: <EnvelopeIcon />,
   nickname: <TagIcon />,
   gender: <UserIcon />,
+  avatar: <UserIcon />,
+  achievement: <ArchiveBoxIcon />,
+  userInfo: <InformationCircleIcon />,
+  chatting: <ChatBubbleBottomCenterTextIcon />,
+  home: <HomeIcon />,
+  speech: <MicrophoneIcon />,
 };
