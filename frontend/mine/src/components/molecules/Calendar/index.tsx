556233091cec44796e468cf8438fb33431789c79
kimdw524 <kimdw524@naver.com>
2024-07-24 17:55:13 +0900

    fe/feat: Calendar ing
    캘린더 만들다가 집감
    
    #40

---
+++ frontend/mine/src/components/molecules/Calendar/index.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { containerCss } from './style';
+
+interface CalendarProps {}
+
+const Calendar = () => {
+  return <div css={containerCss}>달력</div>;
+};
+
+export default Calendar;
---
+++ frontend/mine/src/components/molecules/Calendar/style.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react';
+
+export const containerCss = css`
+  padding: 1rem;
+  border-radius: 0.5rem;
+`;
