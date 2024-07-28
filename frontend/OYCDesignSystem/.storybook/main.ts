c95d0e6739832d6a4a453d6ad0107607859eb9dd
yoha6865 <yoha6865@gmail.com>
2024-07-28 16:47:08 +0900

    fe/chore: react native webview 설정
    react native 프로젝트 생성
     - native cli 로 프로젝트 생성
    
    react-native-webview 를 이용한 테스트
     - 네이버 접속으로 테스트
    
    #53

--- frontend/OYCDesignSystem/.storybook/main.ts
+++ frontend/OYCDesignSystem/.storybook/main.ts
@@ -1,29 +1,29 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
   addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@storybook/addon-themes"
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
   ],
 
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
 
-  staticDirs: ["..\\public"],
+  staticDirs: ['../public'],
 
   docs: {},
 
   typescript: {
-    reactDocgen: "react-docgen-typescript"
-  }
+    reactDocgen: 'react-docgen-typescript',
+  },
 };
 export default config;
