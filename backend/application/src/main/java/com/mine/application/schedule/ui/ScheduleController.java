c0ca13278d4e89059d4da6652014bc6195da2034
권용진 <dydwls9703@naver.com>
2024-07-29 21:30:12 +0900

    be/feat: 일정 domain 영역 구현
    컨트롤러 캘린더에서 일정 검색 메소드 추가 및 service 클래스 이름 변경
    
    #47

--- backend/application/src/main/java/com/mine/application/schedule/ui/ScheduleController.java
+++ backend/application/src/main/java/com/mine/application/schedule/ui/ScheduleController.java
@@ -3,7 +3,7 @@ package com.mine.application.schedule.ui;
 import com.mine.application.schedule.command.application.AddScheduleService;
 import com.mine.application.schedule.command.application.DeleteScheduleService;
 import com.mine.application.schedule.command.application.UpdateScheduleService;
-import com.mine.application.schedule.query.application.GetSchedulesService;
+import com.mine.application.schedule.query.application.ScheduleQueryService;
 import com.mine.application.schedule.ui.dto.AddScheduleRequest;
 import com.mine.application.schedule.ui.dto.GetSchedulesResponse;
 import com.mine.application.schedule.ui.dto.UpdateScheduleRequest;
@@ -22,7 +22,7 @@ import java.util.List;
 @RestController
 public class ScheduleController {
 
-    private final GetSchedulesService getSchedulesService;
+    private final ScheduleQueryService scheduleQueryService;
     private final AddScheduleService addScheduleService;
     private final UpdateScheduleService updateScheduleService;
     private final DeleteScheduleService deleteScheduleService;
@@ -32,8 +32,12 @@ public class ScheduleController {
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate)
     {
-        List<GetSchedulesResponse> response = getSchedulesService.getSchedulesBetweenDates(startDate, endDate);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(scheduleQueryService.getSchedulesBetweenDates(startDate, endDate));
+    }
+
+    @GetMapping("/calendar")
+    public ResponseEntity<List<GetSchedulesResponse>> getSchedulesByContaining(@RequestParam String query) {
+        return ResponseEntity.ok(scheduleQueryService.getSchedulesByContaining(query));
     }
 
     @PostMapping("/calendar")
