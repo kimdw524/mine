aa5fa9b5435fb6d47f2de16c529a6de5b999f010
권용진 <dydwls9703@naver.com>
2024-07-30 14:02:53 +0900

    be/feat: 가계 도메인 구현
    (refactor) GetAccountResponse 필드 id -> accountId 로 이름변경
    
    #58

--- backend/application/src/main/java/com/mine/application/account/ui/dto/GetAccountResponse.java
+++ backend/application/src/main/java/com/mine/application/account/ui/dto/GetAccountResponse.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class GetAccountResponse {
 
-    private Integer id;
+    private Integer accountId;
 
     private Integer spendCategoryId;
 
