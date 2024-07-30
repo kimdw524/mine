b6eee4a9e1333f3f258577758548d5cfc5511d4d
권용진 <dydwls9703@naver.com>
2024-07-30 09:48:45 +0900

    be/fix: 일정 도메인 수정
    (refactor) ScheduleDateRepository 삭제
    
    #60

--- backend/application/src/main/java/com/mine/application/schedule/query/domain/ScheduleDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mine.application.schedule.query.domain;
-
-import org.springframework.data.repository.Repository;
-
-public interface ScheduleDataRepository extends Repository<ScheduleData, Integer> {
-
-
-
-}
