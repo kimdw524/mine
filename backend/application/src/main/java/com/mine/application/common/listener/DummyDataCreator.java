f1e8e13c8df2acc564d689645f78c8c3ce830b1c
김현진 <khj745700@naver.com>
2024-07-30 23:17:30 +0900

    be/feat: dummydatacreator 생성기
    #67

---
+++ backend/application/src/main/java/com/mine/application/common/listener/DummyDataCreator.java
@@ -0,0 +1,49 @@
+package com.mine.application.common.listener;
+
+import com.mine.application.user.command.domain.user.Gender;
+import com.mine.application.user.command.domain.user.Password;
+import com.mine.application.user.command.domain.user.User;
+import com.mine.application.user.command.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+
+@RequiredArgsConstructor
+@Component
+public class DummyDataCreator implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        createUsers();
+    }
+
+
+    private void createUsers() {
+        createUserIfNotNull(1, "khj745700@naver.com", "testtest1", "M", "테스트객체");
+    }
+
+    private void createUserIfNotNull(Integer id, String email, String password, String gender, String nickname) {
+        Optional<User> byId = userRepository.findById(id);
+
+        if(byId.isPresent()) {
+            return;
+        }
+
+        User user = User.builder()
+                .id(id)
+                .email(email)
+                .gender(Gender.of(gender))
+                .password(Password.of(password, false))
+                .nickname(nickname)
+                .build();
+
+        userRepository.save(user);
+    }
+}
