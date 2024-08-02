dfebef4cda353c6153042496698d18348b6d7650
권용진 <dydwls9703@naver.com>
2024-08-02 13:23:27 +0900

    be/fix: 회원가입 시 업적 생성되도록 수정
    회원가입 시 업적 생성되도록 수정
    
    #105

--- backend/application/src/main/java/com/mine/application/achievement/command/application/AddAchievementStatesService.java
+++ backend/application/src/main/java/com/mine/application/achievement/command/application/AddAchievementStatesService.java
@@ -3,10 +3,6 @@ package com.mine.application.achievement.command.application;
 import com.mine.application.achievement.command.domain.AchievementRepository;
 import com.mine.application.achievement.command.domain.AchievementState;
 import com.mine.application.achievement.command.domain.AchievementStateRepository;
-import com.mine.application.common.domain.SessionConstants;
-import com.mine.application.common.domain.SessionDao;
-import com.mine.application.common.erros.errorcode.CommonErrorCode;
-import com.mine.application.common.erros.exception.RestApiException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,15 +13,11 @@ import java.util.stream.IntStream;
 @Service
 public class AddAchievementStatesService {
 
-    private final SessionDao sessionDao;
     private final AchievementRepository achievementRepository;
     private final AchievementStateRepository achievementStateRepository;
 
     @Transactional
-    public void addAchievementStates() {
-        Integer userId = (Integer) sessionDao.get(SessionConstants.USER_ID)
-                .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
-
+    public void addAchievementStates(int userId) {
         IntStream.rangeClosed(1, UpdaterMapper.getAchievementCount())
                 .mapToObj(i -> AchievementState.builder()
                         .userId(userId)
