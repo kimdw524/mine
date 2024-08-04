bd5d6340702a1aa2fe2863491c6b30201a7e45a4
김현진 <khj745700@naver.com>
2024-08-04 18:33:38 +0900

    be/fix: avatar controller cors 설정
    #115

--- backend/application/src/main/java/com/mine/application/avatar/ui/QuestionResController.java
+++ backend/application/src/main/java/com/mine/application/avatar/ui/QuestionResController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(originPatterns = "*", allowedHeaders = "*", allowCredentials = "true")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/avatars/{avatarId}/questions")
