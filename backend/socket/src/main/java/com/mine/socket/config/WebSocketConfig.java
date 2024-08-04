2bab1c39af7132468241ac046e2cc16c861b140a
김현진 <khj745700@naver.com>
2024-08-04 21:37:49 +0900

    be/fix: sockJS() 기능 제외
    #116

--- backend/socket/src/main/java/com/mine/socket/config/WebSocketConfig.java
+++ backend/socket/src/main/java/com/mine/socket/config/WebSocketConfig.java
@@ -8,14 +8,15 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.Session;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 
 @Configuration
-@EnableWebSocketMessageBroker
 @EnableScheduling
+@EnableWebSocket
 public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<Session> {
 
     @Autowired
@@ -30,8 +31,8 @@ public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfig
     @Override
     public void configureStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat") // ex ) ws://localhost:8080/stomp/chat
-                .addInterceptors(loginCheckInterceptor)
-                .setAllowedOriginPatterns("*").withSockJS();
+//                .addInterceptors(loginCheckInterceptor)
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
