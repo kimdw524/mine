a06c60b96c92e098efc0be96570f6ab86378965d
김현진 <khj745700@naver.com>
2024-08-05 14:34:11 +0900

    be/fix: chat 서버 로그인 확인 기능 수정
    #120

--- backend/socket/src/main/java/com/mine/socket/config/SessionConfig.java
+++ backend/socket/src/main/java/com/mine/socket/config/SessionConfig.java
@@ -1,4 +1,4 @@
-package com.mine.application.common.domain;
+package com.mine.socket.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -6,15 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600) // default 값 : 1800
+@EnableRedisHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
-    //TODO : ClusterTopology 설정 필요
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -22,15 +20,17 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
     @Value("${spring.data.redis.port}")
     private Integer port;
 
+
     @Bean
-    public LettuceConnectionFactory connectionFactory() {
+    public LettuceConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port); // ip, port
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
+
 }
