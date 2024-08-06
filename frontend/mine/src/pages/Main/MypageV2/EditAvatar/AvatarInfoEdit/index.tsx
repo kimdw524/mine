a40245d236338f8bf0bbbaf6b9faeab956509c9e
yoha6865 <yoha6865@gmail.com>
2024-08-07 00:12:06 +0900

    fe/fix: success 메시지 수정
    success 메시지 수정
    
    #129

--- frontend/mine/src/pages/Main/MypageV2/EditAvatar/AvatarInfoEdit/index.tsx
+++ frontend/mine/src/pages/Main/MypageV2/EditAvatar/AvatarInfoEdit/index.tsx
@@ -59,7 +59,7 @@ const AvatarInfoEdit = () => {
       notificationContext.handle(
         'contained',
         'success',
-        '이름이 성공적으로 변경되었습니다',
+        '정보가 성공적으로 변경되었습니다',
       );
     },
     onError: () => {
