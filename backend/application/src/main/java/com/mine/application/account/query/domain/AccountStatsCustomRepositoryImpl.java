fa1cc484c875af15c693360b5e222e0902e91a28
권용진 <dydwls9703@naver.com>
2024-08-06 16:20:05 +0900

    be/feat: 통계 기능 구현
    가계 통계 기능 구현
    
    #132

---
+++ backend/application/src/main/java/com/mine/application/account/query/domain/AccountStatsCustomRepositoryImpl.java
@@ -0,0 +1,56 @@
+package com.mine.application.account.query.domain;
+
+import static com.mine.application.account.query.domain.QAccountData.accountData;
+
+import com.mine.application.account.ui.dto.GetSpendAccountStatsResponse;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class AccountStatsCustomRepositoryImpl implements AccountStatsCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<GetSpendAccountStatsResponse> findSpendAccountStats(
+            Integer userId,
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime
+    ) {
+        return jpaQueryFactory
+                .select(Projections.constructor(GetSpendAccountStatsResponse.class,
+                        accountData.spendCategoryId,
+                        accountData.money.sum()))
+                .from(accountData)
+                .where(accountData.dateTime.between(startDateTime, endDateTime)
+                        .and(accountData.userId.eq(userId))
+                        .and(accountData.spendCategoryId.isNotNull()))
+                .groupBy(accountData.spendCategoryId)
+                .orderBy(new OrderSpecifier<>(Order.ASC, accountData.spendCategoryId))
+                .fetch();
+    }
+
+    @Override
+    public Long findIncomeAccountStats(
+            Integer userId,
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime)
+    {
+        return jpaQueryFactory
+                .select(accountData.money.sum())
+                .from(accountData)
+                .where(accountData.dateTime.between(startDateTime, endDateTime)
+                        .and(accountData.userId.eq(userId))
+                        .and(accountData.spendCategoryId.isNull()))
+                .fetchOne();
+    }
+
+}
---
+++ backend/application/src/main/java/com/mine/application/account/ui/AccountStatsController.java
@@ -0,0 +1,41 @@
+package com.mine.application.account.ui;
+
+import com.mine.application.account.query.application.AccountStatsService;
+import com.mine.application.account.ui.dto.GetSpendAccountStatsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequestMapping("/users/statistics/account")
+@RequiredArgsConstructor
+@RestController
+public class AccountStatsController {
+
+    private final AccountStatsService accountStatsService;
+
+    @GetMapping("/spend")
+    public ResponseEntity<List<GetSpendAccountStatsResponse>> getSpendAccountStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate)
+    {
+        return ResponseEntity.ok()
+                .body(accountStatsService.getSpendAccountStats(startDate, endDate));
+    }
+
+    @GetMapping("/income")
+    public ResponseEntity<Long> getIncomeAccountStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate)
+    {
+        return ResponseEntity.ok()
+                .body(accountStatsService.getIncomeAccountStats(startDate, endDate));
+    }
+
+}
