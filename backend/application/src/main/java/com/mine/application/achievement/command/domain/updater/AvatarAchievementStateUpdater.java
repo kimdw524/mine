f3fa033165e66ee1218e7b7ca02df7b16e1f4cd8
권용진 <dydwls9703@naver.com>
2024-08-07 10:09:07 +0900

    be/feat: 업적 업데이트 구현 및 리팩토링
    avatar 업데이터 구현
    
    #80

--- backend/application/src/main/java/com/mine/application/achievement/command/domain/updater/AvatarAchievementStateUpdater.java
+++ backend/application/src/main/java/com/mine/application/achievement/command/domain/updater/AvatarAchievementStateUpdater.java
@@ -1,15 +1,28 @@
 package com.mine.application.achievement.command.domain.updater;
 
 import com.mine.application.achievement.command.domain.AchievementState;
+import com.mine.application.avatar.query.domain.AvatarData;
+import com.mine.application.avatar.query.domain.AvatarDataRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Component
 public class AvatarAchievementStateUpdater implements AchievementStateUpdater {
 
-    // TODO: DB에서 avatar 생성했는지 확인해야함.
+    private final AvatarDataRepository avatarDataRepository;
+
     @Override
     public int updateCount(AchievementState achievementState) {
-        return 1;
+        return updateAvatarCount(achievementState);
+    }
+
+    private int updateAvatarCount(AchievementState achievementState) {
+        int userId = achievementState.getUserId();
+        List<AvatarData> avatars = avatarDataRepository.findAllByUserId(userId);
+        return avatars.isEmpty() ? 0 : 1;
     }
 
 }
